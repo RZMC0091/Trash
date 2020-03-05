a8748ba2fd4ca9925fefca9e9cf7d9e169c6f4cd
RZMC0091 <61288225+RZMC0091@users.noreply.github.com>
2020-03-05 23:32:13 +0300

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(732, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[] {
+        "C:/"};
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 70);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(760, 23);
+            this.metroTextBox1.TabIndex = 0;
+            this.metroTextBox1.Text = "C:/";
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.Click += new System.EventHandler(this.metroTextBox1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(23, 99);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(760, 397);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 41);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Goto";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(104, 41);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(75, 23);
+            this.metroButton3.TabIndex = 4;
+            this.metroButton3.Text = "To C:/";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(185, 41);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.TabIndex = 5;
+            this.metroButton2.Text = "To D:/";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 519);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.metroTextBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Components;
+using System.Diagnostics;
+using System.IO;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MetroStyleManager MetroStyleManager1 = null;
+            this.StyleManager = MetroStyleManager1;
+            treeView1.Nodes.Clear();
+            string[] allfiles = Directory.GetFiles(metroTextBox1.Text);
+            string[] allfolders = Directory.GetDirectories(metroTextBox1.Text);
+            foreach (string filename in allfiles)
+            {
+                treeView1.Nodes.Add(filename);
+            }
+            foreach (string folder in allfolders)
+            {
+                treeView1.Nodes.Add(folder);
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            string[] allfiles = Directory.GetFiles(metroTextBox1.Text);
+            string[] allfolders = Directory.GetDirectories(metroTextBox1.Text);
+            foreach (string filename in allfiles)
+            {
+                treeView1.Nodes.Add(filename);
+            }
+            foreach (string folder in allfolders)
+            {
+                treeView1.Nodes.Add(folder);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            metroTextBox1.Text = treeView1.SelectedNode.Text.ToString();
+            treeView1.Nodes.Clear();
+            string[] allfiles = Directory.GetFiles(metroTextBox1.Text);
+            string[] allfolders = Directory.GetDirectories(metroTextBox1.Text);
+            foreach (string filename in allfiles)
+            {
+                treeView1.Nodes.Add(filename);
+            }
+            foreach (string folder in allfolders)
+            {
+                treeView1.Nodes.Add(folder);
+            }
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text = treeView1.SelectedNode.Text.ToString();
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            string b = "C:/";
+            metroTextBox1.Text = b;
+            treeView1.Nodes.Clear();
+            string[] allfiles = Directory.GetFiles(metroTextBox1.Text);
+            string[] allfolders = Directory.GetDirectories(metroTextBox1.Text);
+            foreach (string filename in allfiles)
+            {
+                treeView1.Nodes.Add(filename);
+            }
+            foreach (string folder in allfolders)
+            {
+                treeView1.Nodes.Add(folder);
+            }
+        }
+
+        private void metroButton2_Click_1(object sender, EventArgs e)
+        {
+            string b = "D:/";
+            metroTextBox1.Text = b;
+            treeView1.Nodes.Clear();
+            string[] allfiles = Directory.GetFiles(metroTextBox1.Text);
+            string[] allfolders = Directory.GetDirectories(metroTextBox1.Text);
+            foreach (string filename in allfiles)
+            {
+                treeView1.Nodes.Add(filename);
+            }
+            foreach (string folder in allfolders)
+            {
+                treeView1.Nodes.Add(folder);
+            }
+        }
+    }
+}
